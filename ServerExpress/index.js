9221dd6534eccd4e85419db5e4e0e1a559710fde
Nguyen Thai Ngoc <ntngoc96>
2019-09-21 16:05:11 +0700

    BE/connect-to-ESP8266

--- ServerExpress/index.js
+++ ServerExpress/index.js
@@ -6,10 +6,10 @@ const admin = require('firebase-admin');
 const port = 8080;
 var app = express();
 var server = app.listen(port, function () {
-  console.log("Node.js server created");
+  console.log(`Node.js server created and listen on port: ${port}`);
 })
 
-
+/* Firebase Firestore Secrect key */
 let serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
@@ -18,38 +18,32 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
-app.use(express.static('front-end'))
-
+/* Start Socket on server */
 var io = socket(server, { pingTimeout: 60000 });
 
 io.on("connection", function (socket) {
   console.log("socket.io connected " + socket.id)
-  io.send("Hello from node.js")
 
-  //connect establish
+  /* Connect to Firestore */
   let docRef = db.collection('users').doc('A0zUndNcLS1FwT6OIRYj').collection('health-monitoring').doc('12-9-2019')
 
-  socket.on("something", function (data) {
-    console.log("Received something")
-    console.log(data)
-  })
-
+  // Listen heart rate data from Python Application
   socket.on("heart_rate", function (data) {
-    console.log("Received heart_rate")
-    console.log(data)
-    docRef.update(data)
+    console.log("Received heart_rate");
+    console.log(data);
+    // Update data
+    docRef.update(data);
+    // Send to ESP8266
+    io.sockets.emit("heart_rate",data.heart_rate);
   })
 
+  // Listen steps - calories - fatburn - meter from Python Application
   socket.on("steps", function (data) {
-    console.log("Received steps and ...")
-    console.log(data)
+    console.log("Received steps and ...");
+    console.log(data);
     docRef.update(data);
   })
 
-  socket.on("message", function (data) {
-    console.log("Received message")
-    console.log(data)
-  })
 })
 
 app.get('/', (req, res) => {
@@ -68,6 +62,7 @@ app.get('/', (req, res) => {
   res.send('doneee');
 })
 
+/* Testing function 
 app.get('/get-data', (req, res) => {
   db.collection('users').get()
     .then((snapshot) => {
@@ -92,12 +87,14 @@ app.get('/send-data', (req, res) => {
 })
 
 app.get('/test-realtime', (req, res) => {
+  let steps = 100;
+  let calories = 0;
+  let fat_burn = 1;
+  let meter = 0;
   setInterval(() => {
-    let heart_rate = Math.ceil(Math.random()*10) + 70
+    let heart_rate = Math.ceil(Math.random()*50) + 70
     db.collection('users').doc('A0zUndNcLS1FwT6OIRYj').collection('health-monitoring').doc('12-9-2019').update({
       heart_rate: heart_rate,
-      steps: '50',
-      calories: '1000'
     })
       .then(function () {
         console.log("Document successfully written!");
@@ -105,8 +102,27 @@ app.get('/test-realtime', (req, res) => {
       .catch(function (error) {
         console.error("Error writing document: ", error);
       });
-  }, 500)
+  }, 2000)
+  setInterval(() => {
+    steps += Math.ceil(Math.random()*10);
+    meter += Math.ceil(Math.random()*8);
+    calories += Math.ceil(Math.random()*15);
+    fat_burn += Math.ceil(Math.random());
+    db.collection('users').doc('A0zUndNcLS1FwT6OIRYj').collection('health-monitoring').doc('12-9-2019').update({
+      steps,
+      fat_burn,
+      calories,
+      meter
+    })
+      .then(function () {
+        console.log("Document successfully written!");
+      })
+      .catch(function (error) {
+        console.error("Error writing document: ", error);
+      });
+  }, 3000)
 
   res.send("real time started")
 })
 
+*/
